dfd9d456c4d3d706f2c9354918ad0875667bb36e
Jeff Segars <jeff@webempoweredchurch.org>
2012-01-30 10:39:18 -0500

    Upgrade to the latest version of wec_flashpresentation

--- typo3conf/ext/wec_flashpresentation/class.tx_wecflashpresentation.php
+++ typo3conf/ext/wec_flashpresentation/class.tx_wecflashpresentation.php
@@ -2,7 +2,7 @@
 /***************************************************************
 * Copyright notice
 *
-* (c) 2006 Foundation for Evangelism
+* (c) 2006-2008 Foundation for Evangelism
 * All rights reserved
 *
 * This file is part of the Web-Empowered Church (WEC)
@@ -26,7 +26,7 @@
 *
 * This copyright notice MUST APPEAR in all copies of the file!
 ***************************************************************/
-/** 
+/**
  * Top level class for the 'wec_flashpresentation' extension.
  *
  * @author		Web-Empowered Church Team <flashpresentation@webempoweredchurch.org>
@@ -37,7 +37,7 @@ require_once(PATH_tslib.'class.tslib_pibase.php');
 require_once(t3lib_extMgm::extPath('wec_flashpresentation').'class.tx_wecflashpresentation_flashobject.php');
 
 
-/** 
+/**
  * Top level class for the 'wec_flashpresentation' extension. Subclasses
  * in pi1 and pi2 are shells that call all functions in parent class.
  *
@@ -49,7 +49,7 @@ class tx_wecflashpresentation extends tslib_pibase {
 	var $prefixId = 'tx_wecflashpresentation';		// Same as class name
 	var $scriptRelPath = 'class.tx_wecflashpresentation.php';	// Path to this script relative to the extension dir.
 	var $extKey = 'wec_flashpresentation';	// The extension key.
-	
+
 	/**
 	 * Main function for the class that passes handling to the pi1 or pi2 function.
 	 * @param	string	Content coming into the extension.
@@ -62,10 +62,10 @@ class tx_wecflashpresentation extends tslib_pibase {
 		$this->pi_setPiVarDefaults();
 		$this->pi_loadLL();
 		$this->pi_initPIflexform(); // Init and get the flexform data of the plugin
-		
+
 		$piFlexForm = $this->cObj->data['pi_flexform']; // Copy the flexform data to a new object
 		$flashConf = array();
-				
+
 		/* Read all conf variables from Typoscript first */
 		foreach($this->conf as $key => $value){
 			if ($key{strlen($key)-1} != ".") {
@@ -74,81 +74,90 @@ class tx_wecflashpresentation extends tslib_pibase {
 			            											 	$key);
 			}
 		}
-		
+
 		/* Pull special values out of flashconf and into own variables.  We don't want do pass these along as FlashVars */
 		$width = $flashConf['width'];
 		$height = $flashConf['height'];
 		$bgcolor = $flashConf['bgcolor'];
 		$flashPath = $flashConf['flashPath'];
-		
-		/* Initialize values for FlashObject */
-		$jsPath = t3lib_extmgm::siteRelPath($this->extKey).'res/';
-		$name = 'wec_flashpresentation_'.$this->cObj->data['uid'];
-		$version = '7';
 
-		/* Create FlashObject class */		
-		$flashObjectClassName = t3lib_div::makeInstanceClassName('tx_wecflashpresentation_flashobject');
-		$flashObject = new $flashObjectClassName($flashPath, $name, $width, $height, $version, $bgcolor, $jsPath);
-		
-		unset($flashConf['userFunc']);
-		unset($flashConf['width']);
-		unset($flashConf['height']);
-		unset($flashConf['bgcolor']);
-		unset($flashConf['flashPath']);
-		
-		$flashObject->addParameter('wmode', $flashConf['wmode']);
-		unset($flashConf['wmode']);
-		
-		/* Combine FlexForm and TS values */
-		if($piFlexForm['data']) {
-			foreach($piFlexForm['data'] as $sheet => $data) {
-				foreach ($data as $lang => $value) {
-					foreach ($value as $key => $val) {
-						/* Skip over the slides field.  We'll perform special processing on this later */
-						//if ($key != "slides") {
+		if($flashPath && (count($flashConf) > 0)) {
+			/* Initialize values for FlashObject */
+			$jsPath = t3lib_extmgm::siteRelPath($this->extKey).'res/';
+			$name = 'wec_flashpresentation_'.$this->cObj->data['uid'].'_'.rand();
+
+			$version = '7';
+
+			$flashObject = t3lib_div::makeInstance('tx_wecflashpresentation_flashobject', $flashPath, $name, $width, $height, $version, $bgcolor, $jsPath);
+
+			unset($flashConf['userFunc']);
+			unset($flashConf['width']);
+			unset($flashConf['height']);
+			unset($flashConf['bgcolor']);
+			unset($flashConf['flashPath']);
+			unset($flashConf['includeLibs']);
+
+			$flashObject->addParameter('wmode', $flashConf['wmode']);
+			unset($flashConf['wmode']);
+
+			/* Combine FlexForm and TS values */
+			if($piFlexForm['data']) {
+				foreach($piFlexForm['data'] as $sheet => $data) {
+					foreach ($data as $lang => $value) {
+						foreach ($value as $key => $val) {
 							$val = $this->pi_getFFvalue($piFlexForm, $key, $sheet);
 							/* If value exists in Flexform, overwrite existing Typoscript value or create new array entry */
-							if ($val != null) {
+							/* @fixme If a content element is reused, the Flexform may include old values that should not be passed through as flashConf */
+							if (($val != null) && (strpos($key, '.') === FALSE)) {
 								$flashConf[$key] = $val;
-							
+
 								/* If bandwidth image comes from Flexform, set bwbase to uploads folder */
 								if ($key == "bwbase") {
 									$flashConf['bwbase'] = "uploads/tx_wecflashpresentation/";
 								}
-			
+
 							}
-						//}	
+						}
 					}
 				}
 			}
-		}
-				
-		$flashConf = array_merge($flashConf, $this->splitSlidesAndTimes($flashConf['slides']));
-		unset($flashConf['slides']);
-		
-		if(!$flashConf['baseurl']) {
-			$flashConf['baseurl'] = t3lib_div::getIndpEnv('TYPO3_SITE_URL');
-		}
-		$flashConf['lastloaded'] = "true";
 
-		/* Add each FlashVar to FlashObject */
-		foreach($flashConf as $var => $value) {
-			$flashObject->addVariable($var, $value);	
-		}
-		$flashObject->write($name);
-		
-		/* Create the output */
-		$html = '<div id="'.$name.'">You do not have the Flash plugin installed, or your browser does not support Javascript (you should enable it, perhaps?)</div>';
-		$javascript = $flashObject->output();	
+			$flashConf = array_merge($flashConf, $this->splitSlidesAndTimes($flashConf['slides']));
+			unset($flashConf['slides']);
 
-		return $this->pi_wrapInBaseClass($html.chr(10).$javascript);
-		
-		
-		return $this->pi_wrapInBaseClass($this->outputHTML($flashPath, $width, $height, $bgcolor, $flashVars));
+			if(!$flashConf['baseurl']) {
+				$flashConf['baseurl'] = t3lib_div::getIndpEnv('TYPO3_SITE_URL');
+			}
+			$flashConf['lastloaded'] = "true";
+
+			/* Add each FlashVar to FlashObject */
+			foreach($flashConf as $var => $value) {
+				$flashObject->addVariable($var, $value);
+			}
+
+			/* Create the output */
+			$flashObject->outputHeader();
+			$html = '<div id="'.$name.'">'.$this->pi_getLL('alt_content').'</div>';
+		} else {
+			$GLOBALS['TSFE']->additionalHeaderData['tx_wecflashplayer_flashobject-error'] = '
+				<style type="text/css">
+					.error {
+						border:solid 1px #CC0000;
+						background:#F7CBCA;
+						color:#CC0000;
+						font-weight:bold;
+						padding:4px;
+						text-align:center;
+					}
+				</style>';
+			$html = '<div class="error">'.$this->pi_getLL('configuration_error').'</div>';
+		}
+
+		return $this->pi_wrapInBaseClass($html);
 	}
-	
+
 	/*
-	 * Implodes both array keys and values.  Taken from example code at php.net 
+	 * Implodes both array keys and values.  Taken from example code at php.net
 	 *
 	 * @param	string	String placed between a key and its corresponding value.
 	 *	@param	string	String placed between one key/value pairing and the next.
@@ -163,10 +172,10 @@ class tx_wecflashpresentation extends tslib_pibase {
 				$output[] = $key.$inner_glue.$item;
 			}
 		}
-		
+
 		return implode($outer_glue,$output);
 	}
-	
+
 	/*
 	 * Reads a string-based slide image/timing pair and splits the values into two
 	 * concatenated strings: one for slide images and one for slide timings.
@@ -174,18 +183,18 @@ class tx_wecflashpresentation extends tslib_pibase {
 	 * @param	string	Slide image and timing pairs.  Images and timing are separated
 	 *					"|" and pairs are separated by a newline.
 	 *	@return	string	Returns a concatenated string with images and timings separated.
-	 */  
+	 */
 	function splitSlidesAndTimes($slides) {
 		$slideArray = explode(chr(10), $slides);
 		$imageArray = array();
 		$timingArray = array();
 		$prevTime = 0;
-		
+
 		foreach ( $slideArray as $slide ) {
-			
+
 			$slide = trim($slide);
 			$splitArray = explode(" ", $slide);
-			
+
 			/* Either slide images or timings are missing */
 			if (count($splitArray)==1) {
 				/* Slide image is present but timing is not */
@@ -202,14 +211,14 @@ class tx_wecflashpresentation extends tslib_pibase {
 				$prevTime .= trim($splitArray[1]);
 			}
 		}
-		
+
 		/* Trim trailing quotes from the end of the strings */
 		$slideStr = trim($slideStr, ',');
 		$timeStr = trim($timeStr, ',');
-		
+
 		return array('slideImages' => $slideStr, 'slideTimes' => $timeStr);
 	}
-	
+
 	/*
 	 * Converts a string into seconds.  Input values are expected to conform
 	 * to HH:MM:SS.
@@ -219,11 +228,11 @@ class tx_wecflashpresentation extends tslib_pibase {
 	 */
 	function getSeconds($str) {
 		$timeArray = explode(":", $str, 3);
-		
+
 		$hour = 0;
 		$min  = 0;
 		$sec  = 0;
-		
+
 		switch(count($timeArray)) {
 			case 1:
 				$sec = $timeArray[0];
@@ -238,7 +247,7 @@ class tx_wecflashpresentation extends tslib_pibase {
 				$sec  = $timeArray[2];
 				break;
 		}
-		
+
 		return ($hour * 3600) + ($min * 60) + $sec;
 	}
 }
--- typo3conf/ext/wec_flashpresentation/ext_emconf.php
+++ typo3conf/ext/wec_flashpresentation/ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "wec_flashpresentation"
+# Extension Manager/Repository config file for ext "wec_flashpresentation".
 #
-# Auto generated 08-11-2007 08:54
+# Auto generated 30-01-2012 10:29
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Flash-based player for displaying synchronized media and presentation slides.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.4.1',
+	'version' => '1.5.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -36,14 +36,14 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'cms' => '',
 			'php' => '3.0.0-0.0.0',
-			'typo3' => '3.8.0-0.0.0',
+			'typo3' => '4.3.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:35:{s:8:"ChangLog";s:4:"a5c2";s:33:"class.tx_wecflashpresentation.php";s:4:"546c";s:45:"class.tx_wecflashpresentation_flashobject.php";s:4:"8d1c";s:12:"ext_icon.gif";s:4:"f772";s:17:"ext_localconf.php";s:4:"d6d7";s:14:"ext_tables.php";s:4:"d241";s:28:"ext_typoscript_constants.txt";s:4:"034d";s:24:"ext_typoscript_setup.txt";s:4:"11de";s:24:"flash-ext_icon-sound.gif";s:4:"d476";s:21:"flexform_ds_audio.xml";s:4:"ba8e";s:21:"flexform_ds_video.xml";s:4:"d975";s:13:"locallang.php";s:4:"30ee";s:13:"locallang.xml";s:4:"bede";s:17:"locallang_csh.php";s:4:"a38f";s:16:"locallang_db.php";s:4:"b7e8";s:16:"locallang_db.xml";s:4:"ae5c";s:28:"wec_flashpresentation.tmproj";s:4:"a604";s:17:"wizard_slides.php";s:4:"1737";s:14:"doc/manual.sxw";s:4:"a344";s:19:"doc/wizard_form.dat";s:4:"e623";s:20:"doc/wizard_form.html";s:4:"3246";s:14:"pi1/ce_wiz.gif";s:4:"43fe";s:41:"pi1/class.tx_wecflashpresentation_pi1.php";s:4:"1e80";s:49:"pi1/class.tx_wecflashpresentation_pi1_wizicon.php";s:4:"7545";s:17:"pi1/locallang.php";s:4:"d41d";s:17:"pi1/locallang.xml";s:4:"cede";s:14:"pi2/ce_wiz.gif";s:4:"9d69";s:41:"pi2/class.tx_wecflashpresentation_pi2.php";s:4:"b250";s:49:"pi2/class.tx_wecflashpresentation_pi2_wizicon.php";s:4:"d14a";s:17:"pi2/locallang.php";s:4:"d41d";s:17:"pi2/locallang.xml";s:4:"5d0b";s:12:"res/100k.jpg";s:4:"e9cc";s:17:"res/mp3_chart.swf";s:4:"9bfa";s:16:"res/swfobject.js";s:4:"66d4";s:19:"res/video_chart.swf";s:4:"186c";}',
+	'_md5_values_when_last_written' => 'a:31:{s:33:"class.tx_wecflashpresentation.php";s:4:"3414";s:45:"class.tx_wecflashpresentation_flashobject.php";s:4:"d2f0";s:12:"ext_icon.gif";s:4:"6d12";s:17:"ext_localconf.php";s:4:"d6d7";s:14:"ext_tables.php";s:4:"4468";s:13:"locallang.xml";s:4:"bede";s:16:"locallang_db.xml";s:4:"ae5c";s:17:"wizard_slides.php";s:4:"1737";s:25:"csh/locallang_csh_pi1.xml";s:4:"54a1";s:25:"csh/locallang_csh_pi2.xml";s:4:"f899";s:14:"doc/manual.sxw";s:4:"b480";s:14:"pi1/ce_wiz.gif";s:4:"c9e9";s:41:"pi1/class.tx_wecflashpresentation_pi1.php";s:4:"1e80";s:49:"pi1/class.tx_wecflashpresentation_pi1_wizicon.php";s:4:"7545";s:25:"pi1/flexform_ds_video.xml";s:4:"12ef";s:17:"pi1/locallang.xml";s:4:"6a39";s:14:"pi2/ce_wiz.gif";s:4:"9e0e";s:41:"pi2/class.tx_wecflashpresentation_pi2.php";s:4:"b250";s:49:"pi2/class.tx_wecflashpresentation_pi2_wizicon.php";s:4:"d14a";s:25:"pi2/flexform_ds_audio.xml";s:4:"bb13";s:17:"pi2/locallang.xml";s:4:"3aac";s:12:"res/100k.jpg";s:4:"e9cc";s:17:"res/mp3_chart.swf";s:4:"9bfa";s:16:"res/swfobject.js";s:4:"b58b";s:19:"res/video_chart.swf";s:4:"186c";s:17:"src/mp3_chart.fla";s:4:"c2c9";s:17:"src/mp3_chart.swf";s:4:"9bfa";s:19:"src/video_chart.fla";s:4:"251f";s:19:"src/video_chart.swf";s:4:"186c";s:20:"static/constants.txt";s:4:"034d";s:16:"static/setup.txt";s:4:"11de";}',
 );
 
 ?>
